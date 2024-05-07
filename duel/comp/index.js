67c2cdda902839eb5c761fd7964030e1316d7e16
JakuWorksOfficial <kubek0823@gmail.com>
2024-05-07 16:57:52 +0200

    updated

---
+++ duel/comp/index.js
@@ -0,0 +1,28 @@
+"use strict";
+let id = 5;
+let company = "Repair Man Industries";
+let isPublished = false;
+let x = "sex";
+let age;
+age = 31;
+let ids = [1, 2, 3, 4];
+let anyArray = [1, 2, "string"];
+let person = [1, 'Jacob', true, [1, 2, 3]];
+let employee;
+employee = [
+    [1, "Jacob"],
+    [2, "Kay"],
+    [3, "Jane"]
+];
+let employee2;
+let pid;
+pid = 'hi';
+pid = 22;
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "up";
+    Direction1["Down"] = "down";
+    Direction1["Left"] = "left";
+    Direction1["Right"] = "right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
